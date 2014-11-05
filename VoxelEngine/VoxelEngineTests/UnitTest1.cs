1e59043c47cfa3cb8c824731b6e82cc0ceb9c285
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2014-11-05 12:43:47 -0700

    Added Main class and test project.

---
+++ VoxelEngine/VoxelEngineTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VoxelEngineTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
